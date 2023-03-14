0e36ba298eea433300ef1e93b27c39515d5a0e29
KhanLewis <khanlewis@hotmail.co.uk>
2023-03-14 16:05:23 +0000

    added board class to print board

---
+++ battleships.py
@@ -0,0 +1,15 @@
+from random import randint
+""" Welcome to my battle ships game"""
+
+class Board:
+    """This generates the board for the game"""
+    def __init__(self, size):
+        self.size = size
+        self.board = []
+        for x in range(size):
+            self.board.append(["~"] * size)
+
+    def print_board(self):
+        """This prints the board"""
+        for row in self.board:
+            print(" ".join(row))
